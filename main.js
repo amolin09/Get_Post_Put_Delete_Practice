4c58ad60f2918779c7e0174a4d64d6d810c491cb
amolin09 <alex.molina5829@gmail.com>
2023-06-01 00:39:48 -0500

    added custom routes for more practice

--- main.js
+++ main.js
@@ -43,6 +43,95 @@ app.get('/games', function(req, res){
   res.json(library)
 })
 
+
+//I added my own route to filter out if a game has been completed or not. does not overwrite the original array
+app.get('/games/completion/:status', function(req, res){
+
+  let completion = req.params.status
+
+  let found = db.find(function(game){
+    if(game.completed == completion){
+      return true
+    }
+    else{
+      return false
+    }
+  })
+
+  if(found){
+    let newdb = db.filter(function(game){
+      if(game.completed == completion){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+
+    let library = newdb.map(function(game){
+      let overview = {}
+      overview.id = game.id
+      overview.title = game.title
+      overview.completed = game.completed
+      overview.rating = game.rating
+      return overview
+    })
+  
+    res.json(library) 
+
+  }
+  else{
+    res.sendStatus(404)
+  }
+  
+  
+})
+
+
+//another custom route to filter games based on their rating
+app.get('/games/rating/:rating', function(req, res){
+
+  let rating = req.params.rating
+
+  let found = db.find(function(game){
+    if(game.rating == rating){
+      return true
+    }
+    else{
+      return false
+    }
+  })
+
+  if(found){
+    let newdb = db.filter(function(game){
+      if(game.rating == rating){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+
+    let library = newdb.map(function(game){
+      let overview = {}
+      overview.id = game.id
+      overview.title = game.title
+      overview.completed = game.completed
+      overview.rating = game.rating
+      return overview
+    })
+  
+    res.json(library) 
+
+  }
+  else{
+    res.sendStatus(404)
+  }
+  
+  
+})
+
+
 app.get('/games/:id', function(req, res){
 
   let id = req.params.id
